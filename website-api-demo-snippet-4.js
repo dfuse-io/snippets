3c17528df0dff80d473c48ecb09052ca5f2c9484
Francois Proulx <42417098+fproulx-eoscanada@users.noreply.github.com>
2019-03-28 16:22:07 -0400

    Update website-api-demo-snippet-4.js

--- website-api-demo-snippet-4.js
+++ website-api-demo-snippet-4.js
@@ -1,1 +1,26 @@
+const { 
+  createDfuseClient, 
+  InboundMessageType,
+} = require("@dfuse/client")
 
+const client = createDfuseClient({ 
+  apiKey: process.env.DFUSE_API_KEY, 
+  network: "mainnet",
+});
+
+client.streamActionTraces({ 
+  account: "eosio.token",
+  action_name: "transfer" 
+}, (message) => {
+  if (message.type === "action_trace") {
+    const { 
+      from, to, quantity, memo 
+    } = message.data.trace.act.data
+    
+    console.log(`${from}->${to} ${quantity} (${memo})`)
+  }
+}).catch((error) => {
+  console.log("An error occurred.", error)
+});
+
+// Click "▶ run" to try this code right here and see `dfuse` in action right now.
