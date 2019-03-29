53f676869d342f105fea86880f48e2006138023d
Francois Proulx <42417098+fproulx-eoscanada@users.noreply.github.com>
2019-03-29 12:28:53 -0400

    Update website-api-demo-snippet-4.js

--- website-api-demo-snippet-4.js
+++ website-api-demo-snippet-4.js
@@ -1,26 +1,21 @@
 const { 
-  createDfuseClient, 
-  InboundMessageType,
-} = require("@dfuse/client")
+  createDfuseClient, InboundMessageType, waitFor
+} = require("@dfuse/client");
 
 const client = createDfuseClient({ 
-  apiKey: process.env.DFUSE_API_KEY, 
-  network: "mainnet",
+  apiKey: process.env.DFUSE_API_KEY, network: "mainnet",
 });
 
-client.streamActionTraces({ 
-  account: "eosio.token",
-  action_name: "transfer" 
-}, (message) => {
-  if (message.type === "action_trace") {
-    const { 
-      from, to, quantity, memo 
-    } = message.data.trace.act.data
-    
-    console.log(`${from}->${to} ${quantity} (${memo})`)
-  }
-}).catch((error) => {
-  console.log("An error occurred.", error)
+const stream = client.streamActionTraces({ 
+  account: "eosio.token", action_name: "transfer"}, (message) => {
+    if (message.type === "action_trace") {
+      const { from, to, quantity, memo } = message.data.trace.act.data
+      console.log(`${from}->${to} ${quantity} (${memo})`);
+    }
 });
+
+await waitFor(5000)
+await stream.unlisten()
+console.log("Stream is closed")
 
 // Click "▶ run" to try this code right here and see `dfuse` in action right now.
