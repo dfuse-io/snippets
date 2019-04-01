0240b1211d9fb2a95a3e3e4651ac89cd40c28507
Matthieu Vachon <matt@eoscanada.com>
2019-04-01 01:12:44 -0400

    Reduce lines of code

--- website-api-demo-snippet-4.js
+++ website-api-demo-snippet-4.js
@@ -1,21 +1,18 @@
-const { 
-  createDfuseClient, InboundMessageType, waitFor
-} = require("@dfuse/client");
+const { createDfuseClient, waitFor } = require("@dfuse/client");
 
 const client = createDfuseClient({ 
   apiKey: process.env.DFUSE_API_KEY, network: "mainnet",
 });
 
-const stream = client.streamActionTraces({ 
-  account: "eosio.token", action_name: "transfer"}, (message) => {
+const data = { account: "eosio.token", action_name: "transfer" }
+const stream = client.streamActionTraces(data, (message) => {
     if (message.type === "action_trace") {
       const { from, to, quantity, memo } = message.data.trace.act.data
-      console.log(`${from}->${to} ${quantity} (${memo})`);
+      console.log(`${from} -> ${to} ${quantity} (${memo})`);
     }
 });
 
 await waitFor(5000)
 await stream.unlisten()
-console.log("Stream is closed")
 
 // Click "▶ run" to try this code right here and see `dfuse` in action right now.
