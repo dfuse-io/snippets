be21d9be1d93629fd81f136cb432851d1a5c1bb9
Matthieu Vachon <matt@eoscanada.com>
2019-04-04 14:38:16 -0400

    Added default value when there is not results for a query

--- website-api-demo-snippet-2.js
+++ website-api-demo-snippet-2.js
@@ -8,6 +8,6 @@ const opts = { limit: 10, sort: "desc" }
 const resp = await client.searchTransactions(`auth:eoscanadacom`, opts)
 
 console.log(`Your last 10 transactions`)
-resp.transactions.map((result) => `https://eosq.app/tx/${result.lifecycle.id}`)
+(resp.transactions || []).map((result) => `https://eosq.app/tx/${result.lifecycle.id}`)
 
 // Click "▶ run" to try this code right here and see `dfuse` in action right now.
