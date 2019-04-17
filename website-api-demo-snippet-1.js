70c6d2b7618b8b0badf8f60f9c4353df6dccee4d
Marc-Antoine Ross <536653+errows@users.noreply.github.com>
2019-04-17 13:23:16 -0400

    fixed undefined print

--- website-api-demo-snippet-1.js
+++ website-api-demo-snippet-1.js
@@ -7,6 +7,6 @@ const client = createDfuseClient({
 const resp = await client.stateTable("eosio.token", "eoscanadacom", "accounts")
 const { balance } = resp.rows[0].json
 
-console.log(`Balance: ${balance} (#${resp.up_to_block_num})`)
+const message = `Balance: ${balance} (#${resp.up_to_block_num})`
 
 // Click "▶ run" to try this code right here and see `dfuse` in action right now.
