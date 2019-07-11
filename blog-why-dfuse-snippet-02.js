040d59498d7a41935bac692c99195779e83d8b32
JC Charlier <info@avionrouge.com>
2019-07-11 12:06:53 -0400

    Update blog-why-dfuse-snippet-02.js

--- blog-why-dfuse-snippet-02.js
+++ blog-why-dfuse-snippet-02.js
@@ -8,7 +8,7 @@ const client = createDfuseClient({
 const opts = { limit: 10, sort: "desc" }
 const resp = await client.searchTransactions("auth:eoscanadacom -action:claimrewards -data.from:eosio.vpay -data.from:eosio.bpay", opts)
 
-console.log(`Your last 10 transactions`);
+console.log(`claimrewards`);
 (resp.transactions || []).map((result) => `https://eosq.app/tx/${result.lifecycle.id}`)
 
 // Click "▶ run" to try this code right here and see `dfuse` in action right now.
