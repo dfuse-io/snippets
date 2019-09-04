d6345cd01387378fe1010429e5177b0a46c79f91
Matthieu Vachon <matt@eoscanada.com>
2019-09-04 16:55:29 -0400

    Second example for ETH

---
+++ website-api-demo-snippet-6.js
@@ -0,0 +1,21 @@
+const { createDfuseClient, waitFor } = require("@dfuse/client")
+
+const config = { apiKey: process.env.DFUSE_API_KEY, network: "mainnet.eth.dfuse.io" }
+const client = createDfuseClient(config);
+
+const stream = await client.graphql(`subscription {
+  searchTransactions(query: "method:\\"transfer(address,uint256)\\"") {
+    node { from to balances:balanceChanges {
+      address new:newValue(encoding: ETHER) old:oldValue(encoding: ETHER)
+    }
+  }}}`, {
+  onMessage: ({ type, data }) => {
+    if (type === 'data') {
+      const { from, to, balances } = data.searchTransactions.node
+      const showBalance = (balance) => `${balance.new-balance.old} (${balance.address})`
+      console.log(`${from} -> ${to} [${balances.map(showBalance).join(", ")}]`)
+  }}
+})
+
+await waitFor(5000)
+await stream.close()
