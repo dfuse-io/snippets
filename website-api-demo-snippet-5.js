d87deec816df044f9c6828d982bdc57e2e00d86e
Matthieu Vachon <matt@eoscanada.com>
2019-09-04 16:11:28 -0400

    Initial ETH example 1

---
+++ website-api-demo-snippet-5.js
@@ -0,0 +1,14 @@
+const { createDfuseClient } = require("@dfuse/client")
+
+const config = { apiKey: process.env.DFUSE_API_KEY, network: "mainnet.eth.dfuse.io" }
+const client = createDfuseClient(config)
+
+const query = "from: b1690c08e213a35ed9bab7b318de14420fb57d8c"
+const resp = await client.graphql(`{
+  searchTransactions(query: "${query}", limit: 10, sort: DESC) {
+    edges { node { hash from to } }
+  }
+}`)
+
+const { edges } = resp.data.searchTransactions
+edges.map(({ node }) => `https://ethq.app/tx/${node.hash}`)
