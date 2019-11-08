9a2c9c6b24ba62361b04c54bd5bac05fae0c3c31
billettc <charles@cbillette.com>
2019-11-08 11:21:32 -0500

    Update website-api-demo-snippet-6.js

--- website-api-demo-snippet-6.js
+++ website-api-demo-snippet-6.js
@@ -7,12 +7,12 @@ const client = createDfuseClient({
 
 const stream = await client.graphql(`subscription {
   searchTransactions(lowBlockNum:-1, indexName:CALLS query: "-value:0") {
-        node { hash matchingCalls { from to value(encoding:ETH) } }
+        node { hash matchingCalls { from to value(encoding:ETHER) } }
   }}`, (message) => {
     if (message.type === "data") {
       const { undo, node: { hash, value, matchingCalls }} = message.data.searchTransactions
       matchingCalls.forEach(({ from, to, value }) => {
-        console.log(`Transfer ${from} -> ${to} [${value} Ether]${undo ? " REVERTED" : ""}`)
+        console.log(`Transfer ${from} -> ${to} [${value} ETH]${undo ? " REVERTED" : ""}`)
       })
     }
   })
