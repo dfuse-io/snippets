41a2cba18227222026a7e98fc0a78d312d85e076
billettc <charles@cbillette.com>
2019-11-08 10:53:53 -0500

    Update website-api-demo-snippet-5.js

--- website-api-demo-snippet-5.js
+++ website-api-demo-snippet-5.js
@@ -7,7 +7,7 @@ const client = createDfuseClient({
 
 const query = "from: b1690c08e213a35ed9bab7b318de14420fb57d8c"
 const resp = await client.graphql(`{
-  searchTransactions(query: "${query}", limit: 10, sort: DESC) {
+  searchTransactions(index:CALLS query: "${query}", limit: 10, sort: DESC) {
     edges { node { hash from to } }
   }
 }`)
